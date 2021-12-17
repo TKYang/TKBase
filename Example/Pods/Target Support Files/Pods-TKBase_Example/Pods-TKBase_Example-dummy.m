4d7ce2a9f14bd4200a1955b2e560336ee47cbf39
TKYang <563047774@qq.com>
2021-12-17 16:43:14 +0800

    initial commit of library
    Signed-off-by: TKYang <563047774@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-TKBase_Example/Pods-TKBase_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TKBase_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TKBase_Example
+@end
---
+++ Example/Pods/Target Support Files/TKBase/TKBase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TKBase : NSObject
+@end
+@implementation PodsDummy_TKBase
+@end
